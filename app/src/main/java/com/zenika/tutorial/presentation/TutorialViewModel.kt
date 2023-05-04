a0d8cb037c3fb494e048a5660a99d976d13fb10b
audreygentili <audrey.gentili@zenika.com>
2023-05-04 10:34:53 +0200

    Update ViewModel to manage the mini-game

---
+++ app/src/main/java/com/zenika/tutorial/presentation/TutorialViewModel.kt
@@ -0,0 +1,75 @@
+package com.zenika.tutorial.presentation
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TutorialViewModel @Inject constructor() : ViewModel() {
+
+    private val _colorsSequence = MutableStateFlow(mutableListOf<String>())
+    val colorsSequence: StateFlow<List<String>> = _colorsSequence
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+            initialValue = listOf("")
+        )
+
+    private var _sequenceSize = MutableStateFlow(0)
+    val sequenceSize: StateFlow<Int> = _sequenceSize
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+            initialValue = 0
+        )
+
+    private var _chestState = MutableStateFlow(false)
+    val chestState: StateFlow<Boolean> = _chestState
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+            initialValue = false
+        )
+
+    private val colorsResult: List<String> = listOf("blue", "green", "red", "yellow")
+
+    fun initColorsSequence() {
+        viewModelScope.launch {
+            _colorsSequence.update { listOf<String>().toMutableList() }
+            _sequenceSize.update { 0 }
+        }
+    }
+
+    fun addColor(color: String) {
+        viewModelScope.launch {
+            _colorsSequence.update { sequence ->
+                sequence.add(color)
+                sequence
+            }
+            _sequenceSize.update { size ->
+                size + 1 }
+        }
+        Log.d("sequence", _colorsSequence.value.toString())
+    }
+
+    fun updateChestState() {
+        viewModelScope.launch {
+            _chestState.update { state ->
+                !state
+            }
+        }
+    }
+
+    fun checkSequence(): Boolean {
+        return _colorsSequence.value[_colorsSequence.value.size - 1] ==
+            colorsResult[_colorsSequence.value.size - 1]
+    }
+}
