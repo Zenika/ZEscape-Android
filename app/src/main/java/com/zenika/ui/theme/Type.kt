8ff3075f51a25e07a3c01d023af8b1cc0b589b57
audreygentili <audrey.gentili@zenika.com>
2023-05-05 10:17:31 +0200

    Add font

--- app/src/main/java/com/zenika/ui/theme/Type.kt
+++ app/src/main/java/com/zenika/ui/theme/Type.kt
@@ -2,16 +2,25 @@ package com.zenika.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.zenika.R
 
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
+        fontFamily = FontFamily(Font(R.font.note_this)),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.note_this)),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp,
     )
 )
