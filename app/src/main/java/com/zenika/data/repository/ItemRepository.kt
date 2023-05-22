4bce70f5bbcf664b5a7203b8b5ecb37358bf9091
audreygentili <audrey.gentili@zenika.com>
2023-05-22 10:07:45 +0200

    Update Inventory init in the UseCase

--- app/src/main/java/com/zenika/data/repository/ItemRepository.kt
+++ app/src/main/java/com/zenika/data/repository/ItemRepository.kt
@@ -1,6 +1,5 @@
 package com.zenika.data.repository
 
-import com.zenika.R
 import com.zenika.data.dao.ItemDao
 import com.zenika.data.model.ItemDto
 import kotlinx.coroutines.flow.Flow
@@ -9,18 +8,10 @@ import javax.inject.Inject
 class ItemRepository @Inject constructor(
     private val dao: ItemDao
 ) {
-    fun getItems(): Flow<List<ItemDto>> = dao.getItems()
+    fun observeItems(): Flow<List<ItemDto>> = dao.observeItems()
 
     suspend fun addItem(itemName: String, itemRes: Int) {
         val item = ItemDto(itemName, itemRes)
         dao.upsertItem(item)
-    }
-
-    suspend fun initInventory() {
-        dao.deleteAll()
-        val parchment = ItemDto("parchment", R.mipmap.parchment)
-        dao.upsertItem(parchment)
-        val paper = ItemDto("paper", R.mipmap.paper)
-        dao.upsertItem(paper)
     }
 }
--- app/src/main/java/com/zenika/tutorial/domain/InitInventoryUseCase.kt
+++ app/src/main/java/com/zenika/tutorial/domain/InitInventoryUseCase.kt
@@ -1,12 +1,26 @@
 package com.zenika.tutorial.domain
 
-import com.zenika.data.repository.ItemRepository
+import com.zenika.R
+import com.zenika.data.dao.ItemDao
+import com.zenika.data.model.ItemDto
 import javax.inject.Inject
 
 class InitInventoryUseCase @Inject constructor(
-    private val itemRepository: ItemRepository
+    private val itemDao: ItemDao
 ) {
     suspend operator fun invoke() {
-        itemRepository.initInventory()
+        deleteAll()
+        upsertInitialItems()
+    }
+
+    private suspend fun deleteAll() {
+        itemDao.deleteAll()
+    }
+
+    private suspend fun upsertInitialItems() {
+        val parchment = ItemDto("parchment", R.mipmap.parchment)
+        itemDao.upsertItem(parchment)
+        val paper = ItemDto("paper", R.mipmap.paper)
+        itemDao.upsertItem(paper)
     }
 }
