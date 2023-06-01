f56d61d4b9122922d86281f8c3ca68a5e5830b4d
audreygentili <audrey.gentili@zenika.com>
2023-06-01 17:12:25 +0200

    Update Navigation with const names

--- app/src/main/java/com/zenika/tutorial/presentation/Tutorial.kt
+++ app/src/main/java/com/zenika/tutorial/presentation/Tutorial.kt
@@ -12,9 +12,19 @@ import com.zenika.tutorial.presentation.instruction.InstructionRoute
 import com.zenika.tutorial.presentation.inventory.InventoryRoute
 import com.zenika.tutorial.presentation.item.ItemRoute
 import com.zenika.tutorial.presentation.main.MainRoute
-import com.zenika.tutorial.presentation.mini_game.MiniGameRoute
+import com.zenika.tutorial.presentation.color_buttons_order_game.ColorButtonsOrderGameRoute
 import com.zenika.tutorial.presentation.parchment.end_parchment.EndParchmentRoute
 import com.zenika.tutorial.presentation.parchment.welcome_parchment.WelcomeParchmentRoute
+
+private const val ROUTE_END_PARCHMENT = "endParchmentRoute"
+private const val ROUTE_INSTRUCTION = "instructionRoute"
+private const val ROUTE_INVENTORY = "inventoryRoute"
+private const val ROUTE_INTRO = "intro"
+private const val ROUTE_MAIN = "mainRoute"
+private const val ROUTE_MINI_GAME = "miniGameRoute"
+private const val ROUTE_WELCOME_PARCHMENT = "welcomeParchmentRoute"
+
+private const val ROUTE_PATTERN_ITEM = "itemRoute/{item}"
 
 @OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.tutorialNavigation(
@@ -23,52 +33,60 @@ fun NavGraphBuilder.tutorialNavigation(
 ) {
     navigation(
         route = route,
-        startDestination = "welcomeParchmentRoute"
+        startDestination = ROUTE_INTRO
     ) {
-        composable("welcomeParchmentRoute") {
+        composable(ROUTE_INTRO) {
             WelcomeParchmentRoute(
                 openInstruction = {
-                    navController.navigate("mainRoute")
-                    navController.navigate("instructionRoute")
+                    navController.popBackStack()
+                    navController.navigate(ROUTE_MAIN)
+                    navController.navigate(ROUTE_INSTRUCTION)
                 }
             )
         }
-        dialog("instructionRoute") {
+        dialog(ROUTE_INSTRUCTION) {
             InstructionRoute(
                 onDismissRequest = {
                     navController.popBackStack()
                 }
             )
         }
-        composable("mainRoute") {
+        composable(ROUTE_MAIN) {
             MainRoute(
                 openMiniGame = {
-                    navController.navigate("miniGameRoute")
+                    navController.navigate(ROUTE_MINI_GAME)
                 },
                 openInventory = {
-                    navController.navigate("inventoryRoute")
+                    navController.navigate(ROUTE_INVENTORY)
                 }
             )
         }
-        dialog("miniGameRoute") {
-            MiniGameRoute(
+        dialog(ROUTE_MINI_GAME) {
+            ColorButtonsOrderGameRoute(
                 onDismissRequest = {
                     navController.popBackStack()
                 }
             )
         }
-        dialog("inventoryRoute") {
+        dialog(ROUTE_INVENTORY) {
             InventoryRoute(
                 onDismissRequest = {
                     navController.popBackStack()
                 },
                 showItem = { item ->
-                    navController.navigate("itemRoute/$item")
+                    navController.navigate(ROUTE_PATTERN_ITEM.replace("{item}", item.toString()))
+                }
+            )
+        }
+        composable(ROUTE_WELCOME_PARCHMENT) {
+            WelcomeParchmentRoute(
+                openInstruction = {
+                    navController.popBackStack(route = ROUTE_MAIN, inclusive = false)
                 }
             )
         }
         dialog(
-            "itemRoute/{item}",
+            ROUTE_PATTERN_ITEM,
             arguments = listOf(navArgument("item") {
                 type = NavType.IntType
             })
@@ -78,17 +96,17 @@ fun NavGraphBuilder.tutorialNavigation(
                     navController.popBackStack()
                 },
                 openWelcomeParchment = {
-                    navController.navigate("welcomeParchmentRoute")
+                    navController.navigate(ROUTE_WELCOME_PARCHMENT)
                 },
                 openEndParchment = {
-                    navController.navigate("endParchmentRoute")
+                    navController.navigate(ROUTE_END_PARCHMENT)
                 }
             )
         }
-        composable("endParchmentRoute") {
+        composable(ROUTE_END_PARCHMENT) {
             EndParchmentRoute(
                 finishGame = {
-                    navController.navigate("mainRoute")
+                    navController.popBackStack()
                 }
             )
         }
