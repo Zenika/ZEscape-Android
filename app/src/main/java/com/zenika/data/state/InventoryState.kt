b5f7ad302902cc46e2e5f5c93b95546107472154
audreygentili <audrey.gentili@zenika.com>
2023-06-01 17:08:57 +0200

    Remove the items in the InventoryState init

--- app/src/main/java/com/zenika/data/state/InventoryState.kt
+++ app/src/main/java/com/zenika/data/state/InventoryState.kt
@@ -1,6 +1,5 @@
 package com.zenika.data.state
 
-import com.zenika.R
 import com.zenika.data.model.ItemDto
 
 class InventoryState(
@@ -8,16 +7,7 @@ class InventoryState(
 ) {
     companion object {
         fun start(): InventoryState = InventoryState(
-            listOf(
-                ItemDto(
-                    "parchment",
-                    R.mipmap.parchment
-                ),
-                ItemDto(
-                    "paper",
-                    R.mipmap.paper
-                )
-            )
+            listOf()
         )
     }
 }
