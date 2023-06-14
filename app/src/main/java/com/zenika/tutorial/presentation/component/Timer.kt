cd2c0ca8d2ea4188ae56a1fc178edf00baff4c6b
audreygentili <audrey.gentili@zenika.com>
2023-06-14 14:38:41 +0200

    Change Timer Composable package

--- app/src/main/java/com/zenika/tutorial/presentation/component/Timer.kt
+++ app/src/main/java/com/zenika/tutorial/presentation/component/Timer.kt
@@ -1,24 +1,25 @@
-package com.zenika.tutorial.presentation.main.component
+package com.zenika.tutorial.presentation.component
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
-fun Timer(remainingTimer: Int) {
+fun Timer(
+    timer: Int,
+    modifier: Modifier
+) {
     Text(
-        text = timerConverter(remainingTimer),
-        Modifier
-            .fillMaxWidth(),
+        text = timerConverter(timer),
+        modifier = modifier,
         textAlign = TextAlign.Center
     )
 }
 
-private fun timerConverter(remainingTimer: Int): String {
-    val minutes = remainingTimer / 60000
-    val seconds = remainingTimer / 1000 % 60
+private fun timerConverter(timer: Int): String {
+    val minutes = timer / 60000
+    val seconds = timer / 1000 % 60
     val secondsString = if (seconds in 0..9) {
         "0$seconds"
     } else {
