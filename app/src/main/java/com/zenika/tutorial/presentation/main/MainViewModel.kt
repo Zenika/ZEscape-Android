199d798e38fd57a645e8c202501d71645435ec07
audreygentili <audrey.gentili@zenika.com>
2023-06-19 16:29:59 +0200

    Change UseCases name and combine States

--- app/src/main/java/com/zenika/tutorial/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/zenika/tutorial/presentation/main/MainViewModel.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zenika.R
 import com.zenika.data.repository.ItemRepository
-import com.zenika.tutorial.domain.GameUIState
-import com.zenika.tutorial.domain.ObserveGameStateUseCase
+import com.zenika.tutorial.domain.ObserveMainGameStateUseCase
+import com.zenika.tutorial.domain.ObserveRemainingTimeUseCase
 import com.zenika.tutorial.domain.StartGameUseCase
 import com.zenika.tutorial.domain.UpdateGameStateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,50 +19,68 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val itemRepository: ItemRepository,
-    private val startGameUseCase: StartGameUseCase,
-    private val updateGameStateUseCase: UpdateGameStateUseCase,
-    observeGameStateUseCase: ObserveGameStateUseCase,
+    private val startGame: StartGameUseCase,
+    private val updateGameState: UpdateGameStateUseCase,
+    observeGameState: ObserveMainGameStateUseCase,
+    observeRemainingTime: ObserveRemainingTimeUseCase
 ) : ViewModel() {
-    val state: StateFlow<GameUIState> = observeGameStateUseCase()
+    val state: StateFlow<MainUiState> = combine(
+        observeGameState(), observeRemainingTime()
+    ) { gameState, remainingTime ->
+        MainUiState(
+            gameState.chestOpened,
+            gameState.mapCollected,
+            gameState.keyCollected,
+            gameState.newItem,
+            remainingTime
+        )
+    }
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
-            initialValue = GameUIState(
+            initialValue = MainUiState(
                 chestOpened = false,
                 mapCollected = false,
                 keyCollected = false,
                 newItem = false,
-                remainingTime = 0
+                remainingTime = 3_600_600
             )
         )
 
     init {
         viewModelScope.launch {
-            startGameUseCase()
+            startGame()
         }
     }
 
     fun collectKey() {
         viewModelScope.launch {
             itemRepository.addItem("key", R.mipmap.key)
-            updateGameStateUseCase.collectKey()
+            updateGameState.collectKey()
         }
     }
 
     fun collectMap() {
         viewModelScope.launch {
             itemRepository.addItem("map", R.mipmap.rolled_map)
-            updateGameStateUseCase.collectMap()
+            updateGameState.collectMap()
         }
     }
 
     fun removeNewItemBadge() {
-        updateGameStateUseCase.removeNewItemBadge()
+        updateGameState.removeNewItemBadge()
     }
 
     fun incrementClueCount() {
-        updateGameStateUseCase.incrementClueCount()
+        updateGameState.incrementClueCount()
     }
 }
 
+class MainUiState(
+    val chestOpened: Boolean,
+    val mapCollected: Boolean,
+    val keyCollected: Boolean,
+    val newItem: Boolean,
+    val remainingTime: Int
+)
 
