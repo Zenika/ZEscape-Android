1c41400cc7faf0eb158a733d3744d6f7342d2c14
audreygentili <audrey.gentili@zenika.com>
2023-06-19 16:27:20 +0200

    Update InitInventory location

--- app/src/main/java/com/zenika/tutorial/domain/StartGameUseCase.kt
+++ app/src/main/java/com/zenika/tutorial/domain/StartGameUseCase.kt
@@ -1,17 +1,37 @@
 package com.zenika.tutorial.domain
 
+import com.zenika.R
+import com.zenika.data.dao.ItemDao
+import com.zenika.data.model.ItemDto
 import com.zenika.data.state.GameState
 import com.zenika.data.timer.TimerServiceManager
 import javax.inject.Inject
 
 class StartGameUseCase @Inject constructor(
-    private val initInventoryUseCase: InitInventoryUseCase,
+    private val itemDao: ItemDao,
     private val gameState: GameState,
     private val timerServiceManager: TimerServiceManager
 ) {
     suspend operator fun invoke() {
-        initInventoryUseCase()
+        initInventory()
         gameState.initGame()
         timerServiceManager.startTimer()
     }
+
+    private suspend fun initInventory() {
+        deleteAll()
+        upsertInitialItems()
+    }
+
+    private suspend fun deleteAll() {
+        itemDao.deleteAll()
+    }
+
+    private suspend fun upsertInitialItems() {
+        val parchment = ItemDto("parchment", R.mipmap.parchment)
+        itemDao.upsertItem(parchment)
+        val paper = ItemDto("paper", R.mipmap.paper)
+        itemDao.upsertItem(paper)
+    }
+
 }
