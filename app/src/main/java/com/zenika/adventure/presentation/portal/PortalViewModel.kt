458b1056fa97fe825e7160f6fc6c741cbb37937e
audreygentili <audrey.gentili@zenika.com>
2023-06-20 17:02:53 +0200

    Update package

--- app/src/main/java/com/zenika/adventure/presentation/portal/PortalViewModel.kt
+++ app/src/main/java/com/zenika/adventure/presentation/portal/PortalViewModel.kt
@@ -2,7 +2,7 @@ package com.zenika.adventure.presentation.portal
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.zenika.tutorial.domain.ObserveRemainingTimeUseCase
+import com.zenika.adventure.domain.ObserveRemainingTimeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
