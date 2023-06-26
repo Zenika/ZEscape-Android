32135614c64094472fccb79211c21ae563c6bfeb
audreygentili <audrey.gentili@zenika.com>
2023-06-26 11:30:18 +0200

    Add Singapore Agency Screen

---
+++ app/src/main/java/com/zenika/adventure/presentation/singapore/agency/SingaporeAgencyViewModel.kt
@@ -0,0 +1,23 @@
+package com.zenika.adventure.presentation.singapore.agency
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zenika.adventure.domain.ObserveRemainingTimeUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class SingaporeAgencyViewModel @Inject constructor(
+    observeRemainingTime: ObserveRemainingTimeUseCase
+) : ViewModel() {
+    val remainingTime: StateFlow<Int> =
+        observeRemainingTime()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+                initialValue = 3_600_600
+            )
+}
