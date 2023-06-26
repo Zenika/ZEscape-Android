1e9f788398109d184bfe576891195765e638d91c
audreygentili <audrey.gentili@zenika.com>
2023-06-26 11:26:43 +0200

    Add Instruction in Singapore agency

---
+++ app/src/main/java/com/zenika/adventure/presentation/singapore/instruction/InstructionSingaporeRoute.kt
@@ -0,0 +1,12 @@
+package com.zenika.adventure.presentation.singapore.instruction
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InstructionSingaporeRoute(
+    onDismissRequest: () -> Unit
+) {
+    InstructionSingaporeDialog(
+        onDismissRequest
+    )
+}
