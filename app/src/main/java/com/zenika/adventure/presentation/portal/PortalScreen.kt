ccb65de9b55c7f1d1b5a15a880848ca2b665c65a
audreygentili <audrey.gentili@zenika.com>
2023-06-27 15:23:20 +0200

    Update ScaffoldScreen content

--- app/src/main/java/com/zenika/adventure/presentation/portal/PortalScreen.kt
+++ app/src/main/java/com/zenika/adventure/presentation/portal/PortalScreen.kt
@@ -1,8 +1,17 @@
 package com.zenika.adventure.presentation.portal
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.zenika.R
+import com.zenika.adventure.presentation.component.ContinentsMap
+import com.zenika.adventure.presentation.component.InventoryBag
 import com.zenika.adventure.presentation.component.ScaffoldScreen
+import com.zenika.ui.theme.screenPadding
 import com.zenika.utils.ScreenPreview
 import com.zenika.utils.ZEscapeThemePreview
 
@@ -13,15 +22,28 @@ fun PortalScreen(
     accessToPortal: () -> Unit,
     openWorldMap: () -> Unit,
     openInventory: () -> Unit,
-    ) {
+) {
     ScaffoldScreen(
         remainingTime = remainingTime,
         goToSettings = goToSettings,
         onClick = accessToPortal,
-        openWorldMap = openWorldMap,
-        openInventory = openInventory,
         background = R.mipmap.background_neon
-    )
+    ) {
+        ContinentsMap(
+            modifier = Modifier
+                .size(100.dp)
+                .align(Alignment.BottomStart)
+                .clickable { openWorldMap() }
+                .padding(screenPadding)
+        )
+        InventoryBag(
+            modifier = Modifier
+                .size(100.dp)
+                .align(Alignment.BottomEnd)
+                .clickable { openInventory() }
+                .padding(screenPadding)
+        )
+    }
 }
 
 @ScreenPreview
