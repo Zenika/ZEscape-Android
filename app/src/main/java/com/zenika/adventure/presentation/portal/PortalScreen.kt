b1a05dd34cdd45de148429b48f25737a8caae486
audreygentili <audrey.gentili@zenika.com>
2023-06-27 11:29:24 +0200

    Implement inventory Adventure

--- app/src/main/java/com/zenika/adventure/presentation/portal/PortalScreen.kt
+++ app/src/main/java/com/zenika/adventure/presentation/portal/PortalScreen.kt
@@ -3,15 +3,15 @@ package com.zenika.adventure.presentation.portal
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.zenika.R
-import com.zenika.adventure.presentation.component.AdventureInventoryBag
-import com.zenika.adventure.presentation.component.ContinentsMap
 import com.zenika.adventure.presentation.component.ScaffoldScreen
 import com.zenika.utils.ScreenPreview
 import com.zenika.utils.ZEscapeThemePreview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.zenika.adventure.presentation.component.AdventureInventoryBag
+import com.zenika.adventure.presentation.component.ContinentsMap
 
 @Composable
 fun PortalScreen(
