46e037709c30f7596cc4adfdcdbdad6db81c300c
audreygentili <audrey.gentili@zenika.com>
2023-06-27 16:18:46 +0200

    Add Animation fadeOut onBalloonsClick

--- app/src/main/java/com/zenika/adventure/presentation/singapore/agency/SingaporeAgencyScreen.kt
+++ app/src/main/java/com/zenika/adventure/presentation/singapore/agency/SingaporeAgencyScreen.kt
@@ -1,7 +1,10 @@
 package com.zenika.adventure.presentation.singapore.agency
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -9,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.zenika.R
 import com.zenika.adventure.presentation.component.ContinentsMap
@@ -34,37 +38,58 @@ fun SingaporeAgencyScreen(
         onClick = {},
         background = R.mipmap.singapore_agency
     ) {
-        if (!state.collectSword) {
+        AnimatedVisibility(
+            !state.collectSword,
+            modifier = Modifier
+                .height(300.dp)
+                .align(Alignment.TopStart)
+                .clickable(
+                    interactionSource = MutableInteractionSource(),
+                    indication = null,
+                    onClick = collectSword
+                )
+                .padding(screenPadding),
+            exit = fadeOut()
+        ) {
             Image(
                 painter = painterResource(id = R.mipmap.green_balloon),
-                contentDescription = "Ballon vert avec épée accrochée au bout",
-                modifier = Modifier
-                    .height(300.dp)
-                    .align(Alignment.TopStart)
-                    .clickable { collectSword() }
-                    .padding(screenPadding)
+                contentDescription = stringResource(R.string.green_balloon)
             )
         }
-        if (!state.collectHook) {
+        AnimatedVisibility(
+            !state.collectHook,
+            modifier = Modifier
+                .height(250.dp)
+                .align(Alignment.TopCenter)
+                .clickable(
+                    interactionSource = MutableInteractionSource(),
+                    indication = null,
+                    onClick = collectHook
+                )
+                .padding(screenPadding),
+            exit = fadeOut()
+        ) {
             Image(
                 painter = painterResource(id = R.mipmap.red_balloon),
-                contentDescription = "Ballon rouge avec crochet accroché au bout",
-                modifier = Modifier
-                    .height(250.dp)
-                    .align(Alignment.TopCenter)
-                    .clickable { collectHook() }
-                    .padding(screenPadding)
+                contentDescription = stringResource(R.string.red_balloon)
             )
         }
-        if (!state.collectSingaporeKey) {
+        AnimatedVisibility(
+            !state.collectSingaporeKey,
+            modifier = Modifier
+                .height(300.dp)
+                .align(Alignment.TopEnd)
+                .clickable(
+                    interactionSource = MutableInteractionSource(),
+                    indication = null,
+                    onClick = collectKey
+                )
+                .padding(screenPadding),
+            exit = fadeOut()
+        ) {
             Image(
                 painter = painterResource(id = R.mipmap.blue_balloon),
-                contentDescription = "Ballon bleu avec clé accrochée au bout",
-                modifier = Modifier
-                    .height(300.dp)
-                    .align(Alignment.TopEnd)
-                    .clickable { collectKey() }
-                    .padding(screenPadding)
+                contentDescription = stringResource(R.string.blue_balloon)
             )
         }
         ContinentsMap(
