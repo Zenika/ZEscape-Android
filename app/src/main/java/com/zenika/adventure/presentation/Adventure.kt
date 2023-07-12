c77622758e991690f56bf7ef6cc02706bfb10734
audreygentili <audrey.gentili@zenika.com>
2023-07-12 10:41:27 +0200

    Fix lines

--- app/src/main/java/com/zenika/adventure/presentation/Adventure.kt
+++ app/src/main/java/com/zenika/adventure/presentation/Adventure.kt
@@ -127,10 +127,6 @@ fun NavGraphBuilder.adventureNavigation(
                 goToSingaporeAgency = {
                     navController.navigate(ROUTE_ON_OFF_GAME)
                     navController.navigate(ROUTE_SINGAPORE_INSTRUCTION)
-                },
-                openOnOffGame = {
-                    navController.navigate(ROUTE_ON_OFF_GAME)
-                    navController.navigate(ROUTE_SINGAPORE_INSTRUCTION)
                 }
             )
         }
