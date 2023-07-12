3d875d0336c2a9f0d4e77fe48ca45ce73cf3fcac
audreygentili <audrey.gentili@zenika.com>
2023-07-12 10:53:47 +0200

    Remove TutorialDialog in adventure

--- app/src/main/java/com/zenika/adventure/presentation/inventory/AdventureInventoryDialog.kt
+++ app/src/main/java/com/zenika/adventure/presentation/inventory/AdventureInventoryDialog.kt
@@ -1,5 +1,8 @@
 package com.zenika.adventure.presentation.inventory
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,12 +17,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.zenika.R
+import com.zenika.adventure.presentation.inventory.component.AdventureInventoryBox
 import com.zenika.data.model.ItemDto
 import com.zenika.data.state.InventoryState
-import com.zenika.tutorial.presentation.component.TutorialDialog
-import com.zenika.adventure.presentation.inventory.component.AdventureInventoryBox
 import com.zenika.ui.theme.dialogPadding
+import com.zenika.ui.theme.screenPadding
 import com.zenika.utils.ScreenPreview
 import com.zenika.utils.ZEscapeThemePreview
 
@@ -30,27 +35,44 @@ fun AdventureInventoryDialog(
     showItem: (Int) -> Unit,
     onDismissRequest: () -> Unit
 ) {
-    TutorialDialog(
-        onDismissRequest = onDismissRequest
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
     ) {
-        Column(
-            modifier
-                .height(580.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(screenPadding)
+                .border(
+                    width = 2.dp,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    shape = MaterialTheme.shapes.extraLarge
+                )
+                .padding(1.dp)
+                .background(
+                    MaterialTheme.colorScheme.primaryContainer,
+                    MaterialTheme.shapes.extraLarge
+                )
+                .padding(dialogPadding)
         ) {
-            Text(
-                text = stringResource(R.string.inventory),
-                Modifier.padding(dialogPadding),
-                style = MaterialTheme.typography.headlineMedium
-            )
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
+            Column(
+                modifier
+                    .height(580.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                items(
-                    items.items
-                ) { item ->
-                    AdventureInventoryBox(item = item.resource, showItem = showItem)
+                Text(
+                    text = stringResource(R.string.inventory),
+                    Modifier.padding(dialogPadding),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                ) {
+                    items(
+                        items.items
+                    ) { item ->
+                        AdventureInventoryBox(item = item.resource, showItem = showItem)
+                    }
                 }
             }
         }
