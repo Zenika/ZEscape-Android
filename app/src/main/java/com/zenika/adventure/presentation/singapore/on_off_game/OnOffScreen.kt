387ffebc775eaaf74cc87908fb732fab6fb9db10
Olivier PEREZ <olivier@olivierperez.fr>
2023-07-18 21:28:33 +0200

    Tweak OnOff ColorButton

--- app/src/main/java/com/zenika/adventure/presentation/singapore/on_off_game/OnOffScreen.kt
+++ app/src/main/java/com/zenika/adventure/presentation/singapore/on_off_game/OnOffScreen.kt
@@ -1,9 +1,10 @@
 package com.zenika.adventure.presentation.singapore.on_off_game
 
-import androidx.compose.animation.Animatable
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.EaseOutCubic
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,17 +13,20 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.unit.dp
 import com.zenika.R
 import com.zenika.adventure.presentation.component.ScaffoldScreen
 import com.zenika.ui.theme.FalseButton
 import com.zenika.ui.theme.TrueButton
+import com.zenika.utils.ScreenPreview
 import com.zenika.utils.ZEscapeThemePreview
 
 @Composable
@@ -47,8 +51,7 @@ fun OnOffScreen(
             items(buttonsList.size) { buttonId ->
                 ColorButton(
                     buttonsList[buttonId],
-                    buttonId,
-                    switchColor
+                    onClick = { switchColor(buttonId) }
                 )
             }
         }
@@ -58,36 +61,35 @@ fun OnOffScreen(
 @Composable
 private fun ColorButton(
     isButtonGreen: Boolean,
-    buttonId: Int,
-    switchColor: (Int) -> Unit
+    onClick: () -> Unit
 ) {
-    val color = remember { Animatable(FalseButton) }
+    val color by animateColorAsState(
+        targetValue = if (isButtonGreen) TrueButton else FalseButton,
+        animationSpec = tween(300, easing = EaseOutCubic),
+        label = "ColorButton's color"
+    )
 
-    LaunchedEffect(isButtonGreen) {
-        color.animateTo(
-            if (isButtonGreen) TrueButton else FalseButton,
-            animationSpec = tween(1000)
-        )
-    }
+    val interactionSource = remember { MutableInteractionSource() }
     Box(
         modifier = Modifier
             .fillMaxSize()
             .clip(RoundedCornerShape(20.dp))
-            .background(color.value)
+            .drawBehind { drawRect(color) }
             .size(100.dp)
-            .clickable { switchColor(buttonId) }
+            .clickable(interactionSource, null, onClick = onClick)
     )
 }
 
-@Preview
+@ScreenPreview
 @Composable
 private fun OnOffScreenPreview() {
     ZEscapeThemePreview {
+        var isGreen by remember { mutableStateOf(true) }
         OnOffScreen(
             goToSettings = {},
             remainingTime = 3600,
-            buttonsList = listOf(true, false, true, false, false, false),
-            switchColor = {}
+            buttonsList = listOf(isGreen, false, true, false, false, false),
+            switchColor = { isGreen = !isGreen }
         )
     }
 }
