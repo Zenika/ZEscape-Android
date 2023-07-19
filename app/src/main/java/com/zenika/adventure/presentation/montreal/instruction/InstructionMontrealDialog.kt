5943cc4848f0eafc8ac547ee593b1557fda9ec86
audreygentili <audrey.gentili@zenika.com>
2023-07-19 08:46:04 +0200

    Add instruction to Montreal agency

---
+++ app/src/main/java/com/zenika/adventure/presentation/montreal/instruction/InstructionMontrealDialog.kt
@@ -0,0 +1,53 @@
+package com.zenika.adventure.presentation.montreal.instruction
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.zenika.R
+import com.zenika.ui.theme.adventureBodyMedium
+import com.zenika.ui.theme.dialogPadding
+import com.zenika.ui.theme.screenPadding
+import com.zenika.utils.ComposablePreview
+import com.zenika.utils.ZEscapeThemePreview
+
+@Composable
+fun InstructionMontrealDialog(
+    onDismissRequest: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
+    ) {
+        Text(
+            text = stringResource(R.string.instruction_montreal),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(screenPadding)
+                .background(
+                    MaterialTheme.colorScheme.primaryContainer,
+                    MaterialTheme.shapes.extraLarge
+                )
+                .padding(dialogPadding),
+            textAlign = TextAlign.Center,
+            style = adventureBodyMedium
+        )
+    }
+}
+
+@ComposablePreview
+@Composable
+private fun InstructionMontrealDialogPreview() {
+    ZEscapeThemePreview {
+        InstructionMontrealDialog(
+            onDismissRequest = {}
+        )
+    }
+}
---
+++ app/src/main/java/com/zenika/adventure/presentation/montreal/instruction/InstructionMontrealRoute.kt
@@ -0,0 +1,12 @@
+package com.zenika.adventure.presentation.montreal.instruction
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InstructionMontrealRoute(
+    onDismissRequest: () -> Unit
+) {
+    InstructionMontrealDialog(
+        onDismissRequest
+    )
+}
