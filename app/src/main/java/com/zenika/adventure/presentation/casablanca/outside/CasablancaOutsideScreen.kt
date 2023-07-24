73b4d042468f5d5ce8f6fd254eee99d18eb4b5a0
audreygentili <audrey.gentili@zenika.com>
2023-07-24 11:29:17 +0200

    Add explication text

--- app/src/main/java/com/zenika/adventure/presentation/casablanca/outside/CasablancaOutsideScreen.kt
+++ app/src/main/java/com/zenika/adventure/presentation/casablanca/outside/CasablancaOutsideScreen.kt
@@ -2,7 +2,7 @@ package com.zenika.adventure.presentation.casablanca.outside
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -42,24 +43,32 @@ fun CasablancaOutsideScreen(
         goToSettings = goToSettings,
         background = R.mipmap.casablanca_outside
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
-            horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally)
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            items(
-                buttonsText.size
-            ) { item ->
-                Button(
-                    onClick = { if (buttonsText[item] == R.string.go_through_window) enterInAgency() },
-                    modifier = Modifier.height(80.dp)
-                ) {
-                    Text(
-                        text = stringResource(buttonsText[item]),
-                        textAlign = TextAlign.Center
-                    )
+            Text(
+                text = stringResource(R.string.casablanca_outside_text),
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
+                horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally)
+            ) {
+                items(
+                    buttonsText.size
+                ) { item ->
+                    Button(
+                        onClick = { if (buttonsText[item] == R.string.go_through_window) enterInAgency() },
+                        modifier = Modifier.height(80.dp)
+                    ) {
+                        Text(
+                            text = stringResource(buttonsText[item]),
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
             }
         }
