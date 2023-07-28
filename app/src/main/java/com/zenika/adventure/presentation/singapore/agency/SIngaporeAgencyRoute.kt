063b6930d8bc1e358d7f73d120316743526cb7f0
Audrey Gentili <audrey.gentili@zenika.com>
2023-07-28 19:10:09 +0200

    Implement Singapore agency

---
+++ app/src/main/java/com/zenika/adventure/presentation/singapore/agency/SIngaporeAgencyRoute.kt
@@ -0,0 +1,27 @@
+package com.zenika.adventure.presentation.singapore.agency
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun SingaporeAgencyRoute(
+    goToSettings: () -> Unit,
+    openWorldMap: () -> Unit,
+    openInventory: () -> Unit,
+    viewModel: SingaporeAgencyViewModel = hiltViewModel(),
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    SingaporeAgencyScreen(
+        state,
+        goToSettings,
+        openWorldMap,
+        openInventory,
+        viewModel::collectKey,
+        viewModel::collectSword,
+        viewModel::collectHook
+    )
+}
+
