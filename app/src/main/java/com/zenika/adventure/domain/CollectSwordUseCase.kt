399eb3c030b21ae35a8f5bda10c8c19ec57f52d0
Olivier Perez <olivier.perez@zenika.com>
2023-07-28 18:53:35 +0200

    Add items to inventory from UseCase

--- app/src/main/java/com/zenika/adventure/domain/CollectSwordUseCase.kt
+++ app/src/main/java/com/zenika/adventure/domain/CollectSwordUseCase.kt
@@ -1,10 +1,16 @@
 package com.zenika.adventure.domain
 
+import com.zenika.R
+import com.zenika.data.repository.ItemRepository
 import com.zenika.data.state.AdventureGameStateManager
 import javax.inject.Inject
 
 class CollectSwordUseCase @Inject constructor(
-    private val gameStateManager: AdventureGameStateManager
+    private val gameStateManager: AdventureGameStateManager,
+    private val itemRepository: ItemRepository,
 ) {
-    operator fun invoke() = gameStateManager.collectSword()
+    suspend operator fun invoke() {
+        gameStateManager.collectSword()
+        itemRepository.addItem(itemName = "sword", itemRes = R.mipmap.sword)
+    }
 }
--- app/src/main/java/com/zenika/adventure/presentation/singapore/agency/SingaporeAgencyViewModel.kt
+++ app/src/main/java/com/zenika/adventure/presentation/singapore/agency/SingaporeAgencyViewModel.kt
@@ -2,13 +2,11 @@ package com.zenika.adventure.presentation.singapore.agency
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.zenika.R
 import com.zenika.adventure.domain.CollectHookUseCase
 import com.zenika.adventure.domain.CollectSingaporeKeyUseCase
 import com.zenika.adventure.domain.CollectSwordUseCase
 import com.zenika.adventure.domain.ObserveAdventureStateUseCase
 import com.zenika.adventure.domain.ObserveRemainingTimeUseCase
-import com.zenika.data.repository.ItemRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -19,7 +17,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SingaporeAgencyViewModel @Inject constructor(
-    private val itemRepository: ItemRepository,
     observeAdventureState: ObserveAdventureStateUseCase,
     observeRemainingTime: ObserveRemainingTimeUseCase,
     private val collectSingaporeKey: CollectSingaporeKeyUseCase,
@@ -51,27 +48,21 @@ class SingaporeAgencyViewModel @Inject constructor(
     fun collectKey() {
         viewModelScope.launch {
             collectSingaporeKey()
-            addItemToInventory("singaporeKey", R.mipmap.singapore_key)
         }
     }
 
     fun collectSword() {
         viewModelScope.launch {
             collectSwordItem()
-            addItemToInventory("sword", R.mipmap.sword)
         }
     }
 
     fun collectHook() {
         viewModelScope.launch {
             collectHookItem()
-            addItemToInventory("hook", R.mipmap.hook)
         }
     }
 
-    private suspend fun addItemToInventory(itemName: String, itemRes: Int) {
-        itemRepository.addItem(itemName = itemName, itemRes = itemRes)
-    }
 }
 
 class SingaporeUiState(
