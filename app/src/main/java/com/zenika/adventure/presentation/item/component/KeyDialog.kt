5480ec346a46ac290256f525a67b44e5dc9f1a9c
audreygentili <audrey.gentili@zenika.com>
2023-08-01 15:03:51 +0200

    Add Dialog when click on key in inventory

---
+++ app/src/main/java/com/zenika/adventure/presentation/item/component/KeyDialog.kt
@@ -0,0 +1,34 @@
+package com.zenika.adventure.presentation.item.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.zenika.R
+import com.zenika.adventure.presentation.component.AdventureDialog
+import com.zenika.ui.theme.adventureBodyMedium
+import com.zenika.utils.ScreenPreview
+import com.zenika.utils.ZEscapeThemePreview
+
+@Composable
+fun KeyDialog(
+    onDismissRequest: () -> Unit
+) {
+    AdventureDialog(onDismissRequest = onDismissRequest) {
+        Text(
+            text = stringResource(R.string.key),
+            textAlign = TextAlign.Center,
+            style = adventureBodyMedium
+        )
+    }
+}
+
+@ScreenPreview
+@Composable
+private fun KeyDialogPreview() {
+    ZEscapeThemePreview {
+        KeyDialog(
+            onDismissRequest = {}
+        )
+    }
+}
