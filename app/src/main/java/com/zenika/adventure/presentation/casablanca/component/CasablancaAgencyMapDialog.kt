0e22f57f401ed5c9f5d85e75b3cb7a6dcb2b478f
Olivier PEREZ <olivier@olivierperez.fr>
2023-08-21 20:45:08 +0200

    Simplify Casablanca agency map

--- app/src/main/java/com/zenika/adventure/presentation/casablanca/component/CasablancaAgencyMapDialog.kt
+++ app/src/main/java/com/zenika/adventure/presentation/casablanca/component/CasablancaAgencyMapDialog.kt
@@ -1,17 +1,12 @@
 package com.zenika.adventure.presentation.casablanca.component
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -34,57 +29,35 @@ fun CasablancaAgencyMapDialog(
         onDismissRequest = onDismissRequest,
         properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
     ) {
-        Column(
-            modifier = modifier,
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box {
-                Image(
-                    painter = painterResource(R.mipmap.casablanca_map),
-                    contentDescription = stringResource(R.string.casablanca_map)
-                )
-                Canvas(
-                    modifier = Modifier
-                        .padding(start = 28.dp, top = 28.dp)
-                        .size(136.dp, 92.dp)
-                        .clickable(onClick = goToOffices)
-                ) {
-                    drawRect(
-                        color = Color.Transparent
-                    )
-                }
-                Canvas(
-                    modifier = Modifier
-                        .padding(start = 192.dp, top = 28.dp)
-                        .size(100.dp, 80.dp)
-                        .clickable(onClick = goToMeetingRoom)
-                ) {
-                    drawRect(
-                        color = Color.Transparent
-                    )
-                }
-                Canvas(
-                    modifier = Modifier
-                        .padding(start = 28.dp, top = 124.dp)
-                        .size(136.dp, 40.dp)
-                        .clickable(onClick = goToRestRoom)
-                ) {
-                    drawRect(
-                        color = Color.Transparent
-                    )
-                }
-                Canvas(
-                    modifier = Modifier
-                        .padding(start = 192.dp, top = 112.dp)
-                        .size(100.dp, 52.dp)
-                        .clickable(onClick = goToKitchen)
-                ) {
-                    drawRect(
-                        color = Color.Transparent
-                    )
-                }
-            }
+        Box(modifier) {
+            Image(
+                painter = painterResource(R.mipmap.casablanca_map),
+                contentDescription = stringResource(R.string.casablanca_map)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(start = 26.dp, top = 28.dp)
+                    .size(140.dp, 92.dp)
+                    .clickable(onClick = goToOffices)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(start = 192.dp, top = 28.dp)
+                    .size(100.dp, 80.dp)
+                    .clickable(onClick = goToMeetingRoom)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(start = 26.dp, top = 124.dp)
+                    .size(140.dp, 40.dp)
+                    .clickable(onClick = goToRestRoom)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(start = 192.dp, top = 112.dp)
+                    .size(100.dp, 52.dp)
+                    .clickable(onClick = goToKitchen)
+            )
         }
     }
 }
