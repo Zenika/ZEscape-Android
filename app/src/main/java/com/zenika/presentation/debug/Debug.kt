26512e640f3335be101c47fbaa7c276dbdefeccf
Olivier PEREZ <olivier@olivierperez.fr>
2023-09-01 21:43:38 +0200

    Add Simon Says to debug menu

--- app/src/main/java/com/zenika/presentation/debug/Debug.kt
+++ app/src/main/java/com/zenika/presentation/debug/Debug.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigation
 import com.google.accompanist.navigation.animation.composable
+import com.zenika.adventure.presentation.montreal.simonsays.SimonSaysRoute
 import com.zenika.adventure.presentation.singapore.on_off_game.OnOffRoute
 import com.zenika.data.Game
 import com.zenika.presentation.qrcodescan.QrCodeScanRoute
@@ -15,6 +16,7 @@ import kotlinx.collections.immutable.persistentListOf
 private const val ROUTE_HOME = "debugHome"
 private const val ROUTE_QRCODE_SCAN = "debugQrCodeScan"
 private const val ROUTE_ON_OFF_GAME = "debugOnOffGame"
+private const val ROUTE_SIMON_SAYS = "debugSimonSays"
 
 @OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.debugNavigation(
@@ -30,6 +32,7 @@ fun NavGraphBuilder.debugNavigation(
                 goBack = { navController.popBackStack() },
                 games = persistentListOf(
                     Game.ON_OFF to { navController.navigate(ROUTE_ON_OFF_GAME) },
+                    Game.SIMON_SAYS to { navController.navigate(ROUTE_SIMON_SAYS) },
                     Game.CASABLANCA to {
                         navController.handleDeepLink(
                             deeplinkIntent("app://zescape/casablanca/outside")
@@ -51,6 +54,12 @@ fun NavGraphBuilder.debugNavigation(
         }
         composable(ROUTE_ON_OFF_GAME) {
             OnOffRoute(
+                winGame = { navController.popBackStack() },
+                goToSettings = {}
+            )
+        }
+        composable(ROUTE_SIMON_SAYS) {
+            SimonSaysRoute(
                 winGame = { navController.popBackStack() },
                 goToSettings = {}
             )
